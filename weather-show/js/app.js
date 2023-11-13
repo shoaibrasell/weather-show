fa8fa4047b44821e6b6e85e28520424c1930e8f8
Shoaib Ullah Rasel <56650099+shoaibrasell@users.noreply.github.com>
2023-11-13 20:17:37 +0600

    Add files via upload

---
+++ weather-show/js/app.js
@@ -0,0 +1,32 @@
+//Do not show api key on your js file
+const API_key = `7bd0242d0c2ae57d4e52346541a70646`;
+
+const loadTemperature = (city) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTemperature(data));
+};
+
+const displayTemperature = (data) => {
+  // const temperature = document.getElementById("temperature");
+  // temperature.innerText = data.main.temp;
+  setInnerTextById('temperature', data.main.temp);
+  setInnerTextById('condition' , data.weather[0].main);
+  console.log(data.weather[0].main);
+};
+
+const setInnerTextById = (id, text) => {
+  const temperature = document.getElementById(id);
+  temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click', function(){
+  const searchField =document.getElementById('search-field');
+  const city = searchField.value;
+  //set city
+  document.getElementById('city').innerText = city;
+  loadTemperature(city);
+})
+
+loadTemperature("dhaka");
